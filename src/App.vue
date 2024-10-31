423c10855de0938e490bae9a26ee678a5af73c1c
ezequiel <quirozarielezequiel@gmail.com>
2024-10-31 15:20:12 -0300

    Clase 04 Miercoles 4 de septiembre del 2024 - Portafolio 4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
-const nombreApellido= "Ezequiel Quiroz";
+import NavBar from './components/icons/NavBar.vue';
 </script>
 
 <template>
   <header>
-    <h1>{{ nombreApellido }}</h1>
-    <p> Bienvenido a Vue 3 </p>
+    <!--Barra de Navegacion-->
+    <NavBar />
+    
   </header>
+  <main>
+    <section id="educacion"><h2>Educación - Cursos</h2></section>
+    <section id="experiencia"><h2>Experiencia</h2></section>
+    <section id="proyectos"><h2>Proyectos</h2></section>
+    <section id="habilidades"><h2>Habilidades</h2></section>
+    <section id="intereses"><h2>intereses</h2></section>
+  </main>
 </template>
 
 <style scoped>
+
 </style>
